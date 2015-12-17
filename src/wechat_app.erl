118c22f0ada09d07fd2ddec06aadf0250907b6dc
Johnson Lau <gatement@gmail.com>
2015-12-17 09:09:51 +0800

    first commit

---
+++ src/wechat_app.erl
@@ -0,0 +1,16 @@
+-module(wechat_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    wechat_sup:start_link().
+
+stop(_State) ->
+    ok.
