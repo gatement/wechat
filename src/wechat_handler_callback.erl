df28414660a0f4a255159a9621102e88b61f5650
Johnson Lau <gatement@gmail.com>
2015-12-26 07:16:38 +0800

    go

--- src/wechat_handler_callback.erl
+++ src/wechat_handler_callback.erl
@@ -86,6 +86,7 @@ handle_msg_text(Req, ToUserName, FromUserName, Content) ->
 			{'Content', [wechat_util:build_xml_text("lll")]}
 		]},
     RespXml = xmerl:export_simple_content([Resp], xmerl_xml),
+    ?LOG_INFO("reply msg, xml=~p~n", [lists:flatten(RespXml)]),
     cowboy_req:reply(200, [], RespXml, Req).
 
 
